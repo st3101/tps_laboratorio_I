cfc5eb76e53bd20474de0b0d30b9f07687ba2560
Santiago Tomás Leonardi <47321301+st3101@users.noreply.github.com>
2021-04-17 01:57:42 -0300

    Add files via upload

---
+++ src/calculos.h
@@ -0,0 +1,16 @@
+/*
+ * calculos.h
+ *
+ *  Created on: 17 abr. 2021
+ *      Author: santi
+ */
+
+#ifndef CALCULOS_H_
+#define CALCULOS_H_
+
+int cal_suma(float numero1,float numero2,float* resultado);
+int cal_resta(float numero1,float numero2,float* resultado);
+int cal_multiplicacion(float numero1,float numero2,float* resultado);
+int cal_divicion(float numero1,float numero2,float* resultado);
+int cal_factorial(float numero,float* resultado);
+#endif /* CALCULOS_H_ */
